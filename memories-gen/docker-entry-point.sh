9a4283c0b5f36b474d52bd424f95aa1023a5517f
julien <muniak.julien@gmail.com>
2023-04-21 22:17:51 +0200

    fix production build

---
+++ memories-gen/docker-entry-point.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+echo '- Copying resources'
+cp -R  ${SRC_MUSIC_FOLDER}* ${MUSIC_FOLDER}
+cp -R  ${SRC_VIDEO_FOLDER}* ${VIDEO_FOLDER}
+
+echo '- Starting service'
+pnpm run start
--- memories-gen/vite.config.ts
+++ memories-gen/vite.config.ts
@@ -5,19 +5,4 @@ export default defineConfig({
   plugins: [
     solid({ssr: false,}),
   ],
-  build: {
-    commonjsOptions: {
-      strictRequires: "debug",
-      // include: ['node_modules/better-sqlite3/build/Release/better_sqlite3.node'],
-      ignoreDynamicRequires: true,
-      // dynamicRequireTargets: [
-      //   'node_modules/better-sqlite3/**/*.node',
-      //   'node_modules/better-sqlite3/build/Release/better_sqlite3.node',
-      // ],
-      // extensions: [
-      //   '.js',
-      //   '.node',
-      // ]
-    }
-  }
 });
