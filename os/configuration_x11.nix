6ffb8f01cd5aa84dd503b44a2e2a0ee801d1f224
julien <muniak.julien@gmail.com>
2023-06-10 22:27:35 +0200

    deactivate sleep in rpi4

--- os/configuration_x11.nix
+++ os/configuration_x11.nix
@@ -152,6 +152,10 @@ in{
     mkdir -p /opt/db
     chmod 777 /opt/db
   '';
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 
   virtualisation = {
     podman = {
